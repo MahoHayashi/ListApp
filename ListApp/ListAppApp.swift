6f35701931b95e154f127b7e6dcc7b2a8838865d
MahoHayashi <is0792vf@ed.ritsumei.ac.jp>
2025-07-16 14:52:03 +0900

    Initial Commit

---
+++ ListApp/ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppApp.swift
+//  ListApp
+//
+//  Created by maho hayashi on 2025/07/16.
+//
+
+import SwiftUI
+
+@main
+struct ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListAppTests/ListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppTests.swift
+//  ListAppTests
+//
+//  Created by maho hayashi on 2025/07/16.
+//
+
+import Testing
+@testable import ListApp
+
+struct ListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
